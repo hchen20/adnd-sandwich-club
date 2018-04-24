8c4dbdc8c5a74f24b9641a3fd68ef11bb16ba9ab
hchen20 <hchen1991@gmail.com>
2018-04-23 23:27:00 -0700

    Implement partially parseSandwichJson function

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,29 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONObject;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = new Sandwich();
+
+        try {
+            JSONObject sandwishJson = new JSONObject(json);
+            JSONObject sandwichName = sandwishJson.getJSONObject("name");
+            String mainName = sandwichName.getString("mainName");
+
+            String orgin = sandwishJson.getString("placeOfOrigin");
+            String description = sandwishJson.getString("description");
+
+            sandwich.setMainName(mainName);
+            sandwich.setPlaceOfOrigin(orgin);
+            sandwich.setDescription(description);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
     }
 }
